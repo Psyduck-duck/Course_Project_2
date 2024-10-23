2cfabc8fe60d9d6fba6232a010b09371a9f843da
Anton Fomenko <antonfomenko1234@gmail.com>
2024-10-23 13:52:27 +0300

    Пробная сборка ApiConnectionHHRU

--- src/API_system.py
+++ src/API_system.py
@@ -3,53 +3,62 @@ import requests
 
 
 class ApiConnection(ABC):
-    """ Абстрактный класс для работы с api ресурсами"""
+    """Абстрактный класс для работы с api ресурсами"""
 
     @abstractmethod
     def __init__(self):
         """Коструктор для определения основных параметров"""
-        self.search_target = None
-        self.url = None
-        self.headers = None
-        self.is_connecting = False
+        pass
 
+    @abstractmethod
     def connect(self):
         """Метод для определения подключения к сервисам"""
-        response = requests.get(self.url, self.headers)
-        if response.status_code != 200:
-            raise ValueError("Check URL or headers")
-        self.is_connecting = True
+        pass
 
-    def define_search_target(self, search_text):
-        self.search_target = search_text
-
-    @property
-    def vacancy_data(self):
-        """Метод - геттер для получения информации по вакансиям"""
-
-        if self.is_connecting:
-            response = requests.get(self.url, self.headers)
-            data = response.json()
-
-            return data
-
-        else:
-            raise ValueError("Connection status is False")
+    @abstractmethod
+    def get_vacancy_data(self, search_text: str):
+        """Метод для получения информации по вакансиям"""
+        pass
 
 
 class ApiConnectionHHRU(ApiConnection):
     """Класс для работы с API сервисами HH.ru"""
 
     def __init__(self):
-        super().__init__()
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__headers = {
             "text": None,
             "search_field": "name",
             "page": 0,
             "per_page": 100,
-            "only_with_salary": True,
-            "period": 1,
+            "only_with_salary": False,
+            "period": None,
         }
+        self.__search_target = None
+        self.__vacancies = []
+        self.is_connecting = False
 
-    def
+    def __connect(self):
+        """метод для определения установки связи c API"""
+        response = requests.get("https://api.hh.ru", self.__headers)
+        if response.status_code != 200:
+            raise ValueError("Check URL or headers")
+        self.is_connecting = True
+
+    def get_vacancy_data(self, search_text: str, per_page: int) -> list:
+        """Метод для получения вакансий"""
+
+        self.__connect()
+        if self.is_connecting:
+            self.__headers["text"] = search_text
+            self.__headers["per_page"] = per_page
+            while self.__headers.get("page") != 20:
+                response = requests.get(self.__url, self.__headers)
+                vacancies = response.json()["items"]
+                self.__vacancies.extend(vacancies)
+                self.__headers["page"] += 1
+
+        else:
+            raise ValueError("Connection status is False")
+
+        return self.__vacancies
