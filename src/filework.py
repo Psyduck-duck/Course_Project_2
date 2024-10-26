71ebfa01cd21c7dffb57d70d9aaa0b2d94ef061b
Anton Fomenko <antonfomenko1234@gmail.com>
2024-10-26 20:13:50 +0300

    промежуточный результат

--- src/filework.py
+++ src/filework.py
@@ -18,7 +18,7 @@ class FileCreator(ABC):
         pass
 
     @abstractmethod
-    def add_data(self):
+    def add_data(self, vacancy_object):
         pass
 
     @abstractmethod
@@ -37,27 +37,57 @@ class FileCreatorJson(FileCreator):
     def __init__(self, filename = "filename.json"):
         """Конструктор для задачи параметров объекта"""
 
-        self.filename = filename
-        self.path_to_file = os.path.join(PATH_TO_DATA_DIRECTORY, self.filename)
+        self.__filename = filename
+        self.__path_to_file = os.path.join(PATH_TO_DATA_DIRECTORY, self.__filename)
+        self.__urls_list = []
+        if self.is_file():
+            with open(self.filename, "r", encoding="utf-8") as file:
+                vacancy_data = json.load(file)
+                for vacancy in vacancy_data:
+                    self.__urls_list.append(vacancy.url)
+
+    @property
+    def filename(self):
+        """геттер для получения имени файла для работы с данными"""
+
+        return self.__filename
+
+    def is_file(self):
+        """проверяет существует ли файл"""
+
+        try:
+            with open(self.__path_to_file, "r", encoding="utf-8"):
+                return True
+        except FileNotFoundError:
+            return False
 
     def create_file(self):
         """метод для создания файла"""
 
-        with open(self.path_to_file, "w", encoding="utf-8"):
+        if self.is_file():
+            raise ValueError("File already is")
+        with open(self.__path_to_file, "w", encoding="utf-8"):
             pass
 
     def get_data(self):
         """метод для получения данных из файла"""
 
-        self.vacancy_data = []
-        with open(self.filename, "r", encoding="utf-8") as file:
-            self.data = json.load(file)
+        self.__vacancy_data = []
+        with open(self.__filename, "r", encoding="utf-8") as file:
+            self.__data = json.load(file)
 
-    def add_data(self, vacancy_objects):
+    def add_data(self, vacancy_object):
         """метод для добавления уникальной вакансии"""
 
-        with open(self.filename, "a", encoding="utf-8") as file:
-            pass
+        if self.is_file():
 
-
-
+            with open(self.__filename, "a", encoding="utf-8") as file:
+            # Добавлять элементы и параллельно добавлять юрл в список юрлов, если такой юрс уже существует,
+            # пропускать вакансию
+                if not vacancy_object.url in self.__urls_list:
+                    json.dump(vacancy_object, file, indent=4, ensure_ascii=False)
+                    self.__urls_list.append(vacancy_object.url)
+                else:
+                    return False
+        else:
+            print("Create file!")
